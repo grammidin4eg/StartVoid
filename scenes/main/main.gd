e4ece86c1de2783bedac64e18459d5f6f10f0841
Karachevtsev Yu Yu <grammidin4eg@gmail.com>
2024-08-30 17:40:08 +0300

    спавн

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -7,3 +7,7 @@ func _on_bullet_area_body_exited(body):
 
 func _on_middle_line_body_entered(body):
 	body.cross_middle()
+
+
+func _on_player_change_heart(new_hearth):
+	%hpLabel.text = str(new_hearth)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,11 +2,14 @@ extends CharacterBody2D
 
 
 const SPEED = 500.0
+const HURT_IMMORTAL = 2
+const CLOSE_HURT = 5
 const BULLET = preload("res://scenes/shots/laser_shot.tscn")
 
 var bullet_timer : Timer = Timer.new()
 var health: int = 10
 var is_visible: bool = true
+var immortal: float = 0
 
 signal change_heart(new_hearth)
 
@@ -36,9 +39,41 @@ func _physics_process(delta):
 		bullet_timer.start()
 		create_bullet($BulletSpawnPoint_1.global_position)
 		create_bullet($BulletSpawnPoint_2.global_position)
+	
+	# Посмотрим, какие есть коллизии
+	if is_immortal():
+		dec_immortal(delta)
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		#var enemy : Enemy = collider as Enemy
+
+		if collider and collider.is_in_group("EnemyBullet"):
+			collider.queue_free()
+			if not is_immortal():
+				hurt(1)
+				set_immortal(HURT_IMMORTAL)
+		
+		if collider and collider.is_in_group("Enemy"):
+			collider.hurt(CLOSE_HURT)
+			if not is_immortal():
+				hurt(1)
+				set_immortal(HURT_IMMORTAL)
 		
 func create_bullet(position):
 	var bullet := BULLET.instantiate()
 	bullet.global_position = position
 	get_parent().add_child(bullet)
 	bullet.shoot_sound()
+
+func is_immortal() -> bool:
+	return immortal > 0
+
+func set_immortal(value: float):
+	immortal = value
+	$Shield.visible = true
+
+func dec_immortal(value: float):
+	immortal -= value
+	if immortal <= 0:
+		$Shield.visible = false
